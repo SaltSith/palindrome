cf71786d6a84aa8a278f93720d1dcc3079582168
SaltSith66 <SaltSith66@gmail.com>
2022-11-13 12:40:01 +0100

    align code

--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -13,11 +13,11 @@ palindrom_check(const char *string, const uint32_t string_len)
 	bool is_palindrome = true;
 
 	for (uint32_t i = 0; i < string_len / 2; i++) {
-        if (string[i] != string[string_len - 1 - i]) {
-            is_palindrome = false;
-            break;
-        }
-    }
+		if (string[i] != string[string_len - 1 - i]) {
+			is_palindrome = false;
+			break;
+		}
+	}
 
 	return is_palindrome ? 0 : -2;
 }
